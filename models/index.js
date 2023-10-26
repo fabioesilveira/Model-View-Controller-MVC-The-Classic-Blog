6c49c70d8f862c848f8a2afda37c787a6df90bed
Fabio Silveira <fabio.eds@hotmail.com>
2023-10-26 01:54:06 -0300

    FixedBug

--- models/index.js
+++ models/index.js
@@ -8,15 +8,18 @@ User.hasMany(Post, {
 });
 
 Post.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
+  onDelete: "CASCADE"
 });
 
 Comment.belongsTo(User, {
   foreignKey: "user_id",
+  onDelete: "CASCADE"
 });
 
 Comment.belongsTo(Post, {
   foreignKey: "post_id", 
+  onDelete: "CASCADE"
 });
 
 Post.hasMany(Comment, {
--- public/js/new-comment.js
+++ public/js/new-comment.js
@@ -1,13 +1,9 @@
-const theClassicBlogCommentFormHandler = async (event) => {
+async function theClassicBlogCommentFormHandler(event) {
     event.preventDefault(); 
   
     const post_id = window.location.pathname.split('/').pop();
-
-    console.log("post_id", post_id);
-  
-    const comment_text = document.querySelector('#text').value.trim();
-  
-    if (content) {
+    const comment_text = document.querySelector('#commentText').value.trim();
+    
       const response = await fetch(`/api/comments`, {
         method: 'POST',
         body: JSON.stringify({ comment_text, post_id }),
@@ -15,17 +11,19 @@ const theClassicBlogCommentFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.reload(); // When successful, reload the same page
+        alert('succeeded in creating a comment.');
+         // When successful, reload the same page
       } else {
         // console.log('Response status:', response.status);
         // console.log('Response text:', await response.text());
         alert('Failed to create a comment.'); // When unsuccessful, show alert
       }
-    }
+
+    document.location.reload();
   };
   
   
   // Event listeners
-  const theClassicBlogCommentForm = document.querySelector('.new-comment-form');
+const theClassicBlogCommentForm = document.querySelector('.new-comment-form');
   
 theClassicBlogCommentForm.addEventListener('submit', theClassicBlogCommentFormHandler);
